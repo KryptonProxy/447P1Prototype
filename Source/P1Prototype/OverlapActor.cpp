cde23e316ad0aaa9ad9b1c445351069acd7c9120
Gary <thrillman2100@gmail.com>
2023-10-05 16:48:01 -0400

    Final C++

---
+++ Source/P1Prototype/OverlapActor.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OverlapActor.h"
+
+// Sets default values
+AOverlapActor::AOverlapActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	boxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollider"));
+	boxCollider->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+}
+
+// Called when the game starts or when spawned
+void AOverlapActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	boxCollider->OnComponentBeginOverlap.AddDynamic(this, &AOverlapActor::OverlapConsolePrint);
+}
+
+// Called every frame
+void AOverlapActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AOverlapActor::OverlapConsolePrint(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Cyan, TEXT("Hello!"));
+}
+
--- Source/P1Prototype/OverlapActor.h
+++ Source/P1Prototype/OverlapActor.h
@@ -4,16 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "MovingPlatform.generated.h"
+#include "Components/BoxComponent.h"
+#include "OverlapActor.generated.h"
 
 UCLASS()
-class P1PROTOTYPE_API AMovingPlatform : public AActor
+class P1PROTOTYPE_API AOverlapActor : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	AMovingPlatform();
+	AOverlapActor();
 
 protected:
 	// Called when the game starts or when spawned
@@ -22,12 +23,10 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	
-	UFUNCTION(BlueprintCallable)
-	void EnableMovement();
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Demo C++ Stuff | Movement")
-	float speed = 0;
+	UFUNCTION()
+	void OverlapConsolePrint(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	bool bIsMovementEnabled = false;
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	UBoxComponent* boxCollider;
 };
